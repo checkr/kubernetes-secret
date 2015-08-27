38bf1f22b8eac28e42a9ac45271d9634e851abf3
Eric Buth <ericdotbuth@gmail.com>
2015-08-27 17:06:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Usage
+
+Specify values manually.
+
+```bash
+echo -e "value 1\nvalue 2" | kubernetes-secret -n test-secret key-1 key-2 | kubectl create -f -
+```
+
+Or provide input as a CSV (or specify a different delimeter).
+
+```bash
+echo -e "key-1,value 1\nkey-2,value 2" | kubernetes-secret -n test-secret -e -d ',' | kubectl create -f -
+```
---
+++ kubernetes-secret.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+var Resource struct {
+	APIVersion string `json:"apiVersion"`
+	Kind       string `json:"kind"`
+	Type       string `json:"type"`
+	Metadata   struct {
+		Name      string `json:"name"`
+		Namespace string `json:"namespace"`
+	} `json:"metadata"`
+	Data map[string]string `json:"data"`
+}
+
+var Env bool
+var Delimiter string
+
+func init() {
+	Resource.APIVersion = "v1"
+	Resource.Kind = "Secret"
+	Resource.Type = "Opaque"
+	Resource.Data = make(map[string]string)
+	flag.StringVar(&Resource.Metadata.Name, "n", "secret", "name")
+	flag.StringVar(&Resource.Metadata.Namespace, "s", "default", "name")
+	flag.BoolVar(&Env, "e", false, "delimited key/value pairs as input")
+	flag.StringVar(&Delimiter, "d", "=", "delimiter (if -e is specified)")
+}
+
+func main() {
+	flag.Parse()
+
+	// Create a buffered reader from stdin.
+	r := bufio.NewReader(os.Stdin)
+
+	// Check if we should read this in as delimited values.
+	if Env {
+
+		for {
+
+			// Get a value by reading bytes from stdin up to the next newline.
+			// (Newline characters aren't allowed in secrets.)
+			line, err := r.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal(err)
+			}
+
+			// Parse the components.
+			components := strings.SplitN(line, Delimiter, 2)
+			if len(components) != 2 {
+				log.Fatalln("invalid input")
+			}
+
+			// Encode and add the value to the resource.
+			Resource.Data[components[0]] = base64.StdEncoding.EncodeToString([]byte(components[1]))
+		}
+	} else {
+
+		// Use the command line arguments as keys.
+		for _, arg := range flag.Args() {
+
+			// Get a value by reading bytes from stdin up to the next newline.
+			// (Newline characters aren't allowed in secrets.)
+			value, err := r.ReadBytes('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Encode and add the value to the resource.
+			Resource.Data[arg] = base64.StdEncoding.EncodeToString(value)
+		}
+	}
+
+	// Encode the resource as JSON and write it to stdout.
+	b, err := json.Marshal(Resource)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(b)
+}
